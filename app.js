3e842987a2bde80483036a6306dded744ffdca02
Andrew <vurtua7@hotmail.com>
2018-01-31 13:17:08 -0500

    pizza

--- app.js
+++ app.js
@@ -4,22 +4,33 @@ var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/";
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 app.set('views',path.resolve(__dirname,"views"));
 app.set('view engine', 'ejs');
 
-var entries = [];
-// var mongoEntries = {title:"", body:"", date:""};
+// var entries = [];
 
-app.locals.entries = entries;
+// app.locals.entries = entries;
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get("/", (req,res) => 
 {
-    res.render("index");
+    MongoClient.connect(url, (e,db) =>
+    {
+        if(e)throw e;
+        var dbObj = db.db("Games");
+
+        dbObj.collection("games").find().toArray((e, results) =>
+        {
+            console.log("Site Served");
+            db.close();
+            res.render("index", {games:results});
+        });
+    });
+   
 });
 
 app.get("/new-entry", (req,res) => 
@@ -34,30 +45,29 @@ app.post("/new-entry", (req, res) =>
         res.status(400).send("Entries must have some input!");
         return;
     }
-    entries.push(
-        {
-            title:req.body.title,
-            body:req.body.body,
-            published: new Date()
+    // entries.push(
+    //     {
+    //         title:req.body.title,
+    //         body:req.body.body,
+    //         published: new Date()
             
-        });
+    //     });
     // mongoEntries.title = req.body.title;
     // mongoEntries.body = req.body.body;
     
-    // MongoClient.connect(url, (e,db) =>
-    // {
-    //     if(e) throw e;
+    MongoClient.connect(url, (e,db) =>
+    {
+        if(e) throw e;
 
-    //     var dbObj = db.db("TestDB");
+        var dbObj = db.db("Games");
 
-    //     dbObj.collection("games").insertMany(mongoEntries, (e,res) =>
-    //     {
-    //          if(e)throw e;
-    //          console.log("Number of entries: " + res.insertedCount);
-    //         db.close();
-    //      });
-    // })
-    res.redirect("/");
+        dbObj.collection("games").save(req.body, (e,result) =>
+        {
+             console.log("Data saved " + result);
+             db.close();
+             res.redirect("/");
+         });
+    })
 });
 
 app.use((req, res) =>
